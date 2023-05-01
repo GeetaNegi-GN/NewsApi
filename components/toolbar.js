47000d9ae7c5849e48df35195340807e1246a82c
Geeta Negi <geetanegi550@gmail.com>
2023-05-01 22:59:18 +0530

    intial commit

---
+++ components/toolbar.js
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import styles from '../styles/Toolbar.module.css';
+
+export const Toolbar = ()=> {
+    const router = useRouter ();
+    return (
+        <div className={styles.main}>
+            <div onClick = {()=> router.push('/')}>Home</div>
+            <div onClick = {()=> router.push('/feed/1')}>Feed</div>
+            <div onClick = {()=> window.location.href='https://www.linkedin.com/in/geeta-negi-615420249'}>LinkedIn</div>
+
+        </div>
+    );
+};
